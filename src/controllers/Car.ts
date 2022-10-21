e7bad32df9e6f05b54216f0d8453ac6790f999ea
Yuri-RC <cttyuriresende@gmail.com>
2022-10-21 18:31:23 -0300

    feat: car controller

---
+++ src/controllers/Car.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import IService from '../interfaces/IService';
+import { ICar } from '../interfaces/ICar';
+
+export default class CarController {
+  constructor(private _service: IService<ICar>) { }
+
+  public async create(req: Request, res: Response<ICar>) {
+    const result = await this._service.create(req.body);
+    return res.status(201).json(result);
+  }
+}
