c59c0a7e124968dd3df62fe2a725c56e1fdf5802
Yuri-RC <cttyuriresende@gmail.com>
2022-10-21 18:32:04 -0300

    feat: app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import 'express-async-errors';
+import carRouter from './routes/car.routes';
+import errorHandler from './middlewares/erros';
 
 const app = express();
+app.use(express.json());
+app.use(carRouter);
+app.use(errorHandler);
 
 export default app;
